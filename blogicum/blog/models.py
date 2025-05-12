a9fcd0888e531ce7b567ae86123c6e62a0249400
Nikolay-S <grueger@yandex.ru>
2025-05-12 13:06:43 +0300

    Начало великих правок-3

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -84,7 +84,7 @@ class Post(models.Model):
     pub_date = models.DateTimeField(
         null=False,
         verbose_name='Дата и время публикации',
-        help_text='Если установить дату и время в будущем — ' \
+        help_text='Если установить дату и время в будущем — '
         'можно делать отложенные публикации.'
     )
     author = models.ForeignKey(
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -7,9 +7,10 @@ from .models import Category, Post
 def index(request):
     template = 'blog/index.html'
     post_list = Post.objects.select_related('category').filter(
-        category__is_published=True, is_published=True,
+        category__is_published=True,
+        is_published=True,
         pub_date__lt=timezone.now()
-        ).order_by('-pub_date')[0:5]
+    ).order_by('-pub_date')[0:5]
     context = {'post_list': post_list}
     return render(request, template, context)
 
@@ -22,9 +23,7 @@ def post_detail(request, id):
         pub_date__lt=timezone.now(),
         is_published=True,
         category__is_published=True
-        )
-        .select_related('author', 'category')
-        )
+    ).select_related('author', 'category'))
 
     context = {'post': post}
     return render(request, template, context)
