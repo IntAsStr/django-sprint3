6fa4d6dd90ca3b75d3693a0234ce41138b9a1306
Nikolay-S <grueger@yandex.ru>
2025-05-12 12:58:34 +0300

    Начало великих правок

---
+++ blogicum/blog/models.py
@@ -0,0 +1,126 @@
+from django.db import models
+
+from django.contrib.auth import get_user_model
+
+
+class Category(models.Model):
+    title = models.CharField(
+        max_length=256,
+        null=False,
+        verbose_name='Заголовок'
+        )
+    description = models.TextField(
+        null=False,
+        verbose_name='Описание'
+        )
+    slug = models.SlugField(
+        unique=True,
+        null=False,
+        verbose_name='Идентификатор',
+        help_text=(
+            'Идентификатор страницы для URL; '
+            'разрешены символы латиницы, цифры,'
+            ' дефис и подчёркивание.'
+            )
+        )
+    is_published = models.BooleanField(
+        default=True,
+        null=False,
+        verbose_name='Опубликовано',
+        help_text='Снимите галочку, чтобы скрыть публикацию.'
+        )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        null=False,
+        verbose_name='Добавлено'
+        )
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.title
+
+
+class Location(models.Model):
+    name = models.CharField(
+        max_length=256,
+        null=False,
+        verbose_name='Название места'
+        )
+    is_published = models.BooleanField(
+        default=True,
+        null=False,
+        verbose_name='Опубликовано'
+        )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        null=False,
+        verbose_name='Добавлено'
+        )
+
+    class Meta:
+        verbose_name = 'местоположение'
+        verbose_name_plural = 'Местоположения'
+
+    def __str__(self):
+        return self.name
+
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    title = models.CharField(
+        max_length=256,
+        null=False,
+        verbose_name='Заголовок'
+        )
+    text = models.TextField(
+        null=False,
+        verbose_name='Текст'
+        )
+    pub_date = models.DateTimeField(
+        null=False,
+        verbose_name='Дата и время публикации',
+        help_text='Если установить дату и время в будущем — ' \
+        'можно делать отложенные публикации.'
+        )
+    author = models.ForeignKey(
+        User,
+        null=False,
+        on_delete=models.CASCADE,
+        verbose_name='Автор публикации'
+        )
+    location = models.ForeignKey(
+        Location,
+        null=True,
+        on_delete=models.SET_NULL,
+        verbose_name='Местоположение'
+        )
+    category = models.ForeignKey(
+        Category,
+        null=True,
+        on_delete=models.SET_NULL,
+        blank=False,
+        verbose_name='Категория'
+        )
+    is_published = models.BooleanField(
+        default=True,
+        null=False,
+        verbose_name='Опубликовано',
+        help_text='Снимите галочку, чтобы скрыть публикацию.'
+        )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        null=False,
+        verbose_name='Добавлено'
+        )
+
+    class Meta:
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
+
+    def __str__(self):
+        return self.title
---
+++ blogicum/blog/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import get_object_or_404, render
+from django.utils import timezone
+
+from .models import Category, Post
+
+
+def index(request):
+    template = 'blog/index.html'
+    post_list = Post.objects.select_related('category').filter(
+        category__is_published=True, is_published=True,
+        pub_date__lt=timezone.now()
+        ).order_by('-pub_date')[0:5]
+    context = {'post_list': post_list}
+    return render(request, template, context)
+
+
+def post_detail(request, id):
+    template = 'blog/detail.html'
+
+    post = get_object_or_404(Post.objects.filter(
+        id=id,
+        pub_date__lt=timezone.now(),
+        is_published=True,
+        category__is_published=True
+        )
+        .select_related('author', 'category')
+        )
+
+    context = {'post': post}
+    return render(request, template, context)
+
+
+def category_post(request, category_slug) -> str:
+    template = 'blog/category.html'
+
+    category = get_object_or_404(
+        Category,
+        is_published=True,
+        slug=category_slug,
+    )
+
+    post_list = Post.objects.filter(
+        category=category,
+        is_published=True,
+        pub_date__lt=timezone.now()
+    ).select_related('author', 'category')
+
+    context = {
+        'category': category,
+        'post_list': post_list,
+    }
+    return render(request, template, context)
